06d76ba7514ee8814d861dc86f1bdffc10c61337
JinWook <qwert4932@naver.com>
2021-11-22 15:30:49 +0900

    Initial commit

---
+++ src/test/java/com/jenkim/DevjinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevjinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
